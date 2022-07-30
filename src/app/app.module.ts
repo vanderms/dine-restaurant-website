a93fad7331ce1f2ad96a915e4579b613dbfae752
vanderms <vanderms.84@gmail.com>
2022-07-30 20:32:37 -0300

    refactor: remove navbar component. Logo will be inserted on header sections of the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BookingPageComponent } from './pages/booking-page/booking-page.component';
-import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { HeaderSectionComponent } from './components/sections/header-section/header-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     BookingPageComponent,
-    NavbarComponent
+    HeaderSectionComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
