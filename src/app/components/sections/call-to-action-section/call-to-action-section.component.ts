783823dedead26a20f447b0d342ffc47d0e162d8
vanderms <vanderms.84@gmail.com>
2022-07-31 19:34:20 -0300

    feat: implement call to action section responsive design

--- src/app/components/sections/call-to-action-section/call-to-action-section.component.ts
+++ src/app/components/sections/call-to-action-section/call-to-action-section.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UniqueIdService } from 'src/app/services/unique-id/unique-id.service';
 
 @Component({
   selector: 'app-call-to-action-section',
   templateUrl: './call-to-action-section.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class CallToActionSectionComponent implements OnInit {
+  titleId: string = '';
 
-  constructor() { }
+  constructor(private uniqueId: UniqueIdService) {}
 
   ngOnInit(): void {
+    this.titleId = this.uniqueId.generate();
   }
 
+  getSrcset(viewport: 'desktop' | 'tablet' | 'mobile'): string {
+    return (
+      `assets/homepage/ready-bg-${viewport}.jpg 1x,` +
+      `assets/homepage/ready-bg-${viewport}@2x.jpg 2x`
+    );
+  }
 }
